964734339624d9a209a1a0a62c9eeba10ea5908e
Felipe Martins <fe.mmo515@gmail.com>
2025-07-15 20:38:45 -0300

    ajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
